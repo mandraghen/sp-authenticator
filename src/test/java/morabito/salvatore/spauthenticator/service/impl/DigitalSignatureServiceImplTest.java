14b828867ba0d2a9a098e56a0d742b024376196e
Salvatore Morabito <mrb.salvatore@gmail.com>
2023-11-19 21:18:36 +0100

    Introduced test auth API.

---
+++ src/test/java/morabito/salvatore/spauthenticator/service/impl/DigitalSignatureServiceImplTest.java
@@ -0,0 +1,17 @@
+package morabito.salvatore.spauthenticator.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DigitalSignatureServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createSignHeaders() {
+    }
+}
